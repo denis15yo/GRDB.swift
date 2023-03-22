9be929ed7bf19a01e56b5af1792026d345ac9c5a
Denis Shilovich <denis.shilovich@mobyrix.com>
2023-03-22 17:28:06 +0300

    fixes

--- GRDB/Export.swift
+++ GRDB/Export.swift
@@ -1,6 +1,5 @@
 // Export the underlying SQLite library
 #if SWIFT_PACKAGE
-@_exported import CSQLite
 #elseif GRDBCIPHER
 @_exported import SQLCipher
 #elseif !GRDBCUSTOMSQLITE && !GRDBCIPHER
--- Package.swift
+++ Package.swift
@@ -37,18 +37,14 @@ let package = Package(
         .watchOS(.v4),
     ],
     products: [
-        .library(name: "CSQLite", targets: ["CSQLite"]),
         .library(name: "GRDB", targets: ["GRDB"]),
         .library(name: "GRDB-dynamic", type: .dynamic, targets: ["GRDB"]),
     ],
     dependencies: dependencies,
     targets: [
-        .systemLibrary(
-            name: "CSQLite",
-            providers: [.apt(["libsqlite3-dev"])]),
         .target(
             name: "GRDB",
-            dependencies: ["CSQLite"],
+            dependencies: [],
             path: "GRDB",
             cSettings: cSettings,
             swiftSettings: swiftSettings),
